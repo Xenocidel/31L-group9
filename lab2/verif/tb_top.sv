783f05e0b0ac831556b52b1f87761b44dfde82da
Xenocidel <aliao4@uci.edu>
2015-10-29 19:02:09 -0700

    updated tb_top
    see line 37

--- lab2/verif/tb_top.sv
+++ lab2/verif/tb_top.sv
@@ -1,7 +1,7 @@
 
 /* Example 4.12 */
 
- import "DPI-C" context encoder= function void encoder(input int data, output logic [6:0] outData,  output int parity );
+import "DPI-C" context encoder= function void encoder(input int data, output logic [10:0] outData,  output int parity );
 
 
 module dpi_example ();
@@ -34,23 +34,28 @@ module dpi_example ();
       rtl_data_in = $urandom % 2048; //generate 11 random bits
       $display("rtl_in: %b", rtl_data_in);      //display the random data 
       encoder(rtl_data_in, rtl_data_out, parity_out); // feed the data to c programming
-	  //concatenate parity bits and data bits to exp_data_out = 
+	  exp_data_out = parity_out[0:2], rtl_data_out[0:1], parity_out[3], rtl_data_out[2:6], parity_out[4], rtl_data_out[7:14] //might be reversed due to it being defined as DOWNTO, please check!
    end
    
    // generate the rtl output
    ham1511_encode L1( .d(rtl_data_in), .o(rtl_data_out));  // make sure you are using your entity name and port name
    ham1511_decode L2( .i(exp_data_out), .out_vec(decoder_data_out) );
 
-   //compare expected output and rtl output
+   //compare expected output and encoder output
    always @(negedge clk) begin
-       if (...........) begin
-           $display("%t: PASS, Input: %b, Output (EXP/RTL) (%b/%b)", $time, ...............);
+       if (exp_data_out = rtl_data_out) begin
+           $display("%t: PASS, Encoder Input: %b, Output (Expected/RTL) (%b/%b)", $time, $rtl_data_in, $exp_data_out, $rtl_data_out);
        end else begin
-           $display("%t: FAIL, Input: %b, Output (EXP/RTL) (%b/%b)", $time, ...............);
+           $display("%t: FAIL, Encoder Input: %b, Output (Expected/RTL) (%b/%b)", $time, $rtl_data_in, $exp_data_out, $rtl_data_out);
        end
    end
+   //compare original data and decoder output
    always @(decoder_data_out) begin
-        .......  // try yourself for decoder 
+       if (decoder_data_out = rtl_data_in) begin
+           $display("%t: PASS, Decoder Input: %b, Output (Expected/RTL) (%b/%b)", $time, $exp_data_out, $rtl_data_in, $decoder_data_out);
+       end else begin
+           $display("%t: FAIL, Decoder Input: %b, Output (Expected/RTL) (%b/%b)", $time, $exp_data_out, $rtl_data_in, $decoder_data_out);
+       end 
 
  
 endmodule
