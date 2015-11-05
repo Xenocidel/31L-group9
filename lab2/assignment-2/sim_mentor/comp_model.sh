c850a050cf7e2d399d1008e98fc859f7e56d4cb0
Xenocidel <aliao4@uci.edu>
2015-11-04 17:31:45 -0800

    replace lab2 with assignment-2
    deleted old components

--- lab2/assignment-2/sim_mentor/comp_model.sh
+++
@@ -1,8 +0,0 @@
-
-cd $sim
-rm -rf so_libs
-mkdir so_libs
-gcc -shared -m64 -fPIC -O3 $model/hamming.c \
-                           -I$QUESTA_HOME/include \
-                           -o $sim/so_libs/TEST.so
-
