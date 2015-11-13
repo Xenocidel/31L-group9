b095b9a49f42c2ca58a7cf120df6ccf763c9f96b
Xenocidel <aliao4@uci.edu>
2015-11-12 19:40:03 -0800

    added addsub and register modules
    completed, but may contain errors

--- assignment-3/design/reg.vhd
+++ assignment-3/design/reg.vhd
@@ -17,5 +17,20 @@ END reg;
 ARCHITECTURE Structural OF reg IS
 	
 	BEGIN
+		PROCESS (clk, rst_a)
+		VARIABLE temp: std_logic_vector(NBIT-1 downto 0); 
+		BEGIN
+			if rst_a='1' then
+				dout <= OTHERS=>'0';
+			elsif clk'event and clk='1' then
+				if rst_s='1' then
+					dout <= OTHERS=>'0';
+				elsif inc='1' and we='1' then
+					temp:=dout+'1';
+					dout <= temp;
+				elsif we='1' then
+					dout <= din;
+				end if
+			end	if
 	
 END Structural;
