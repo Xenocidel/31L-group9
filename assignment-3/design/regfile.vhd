8d39a429d8064a545d3924dbbf9a1f7c8f8ff710
Werdna629 <amehta@Andrews-MacBook-Pro.local>
2015-11-16 16:13:36 -0800

    Started new regfile
    Created structure, reset is pretty much there now we just need to do
    the reading/writing part

--- assignment-3/design/regfile.vhd
+++ assignment-3/design/regfile.vhd
@@ -19,39 +19,20 @@ END regfile ;
 
 ARCHITECTURE Structural OF regfile IS
 	
-	SIGNAL dec_out : std_logic_vector(2**NSEL-1 downto 0);
 	type reg_to_array is array (2**NSEL-1 downto 0) of std_logic_vector(NBIT-1 downto 0); 
-	SIGNAL mux_in : reg_to_array;
-
-	component reg
-	port (clk, rst_a, rst_s, inc, we: IN std_logic ;
-		din : IN std_logic_vector (NBIT -1 downto 0);
-		dout : OUT std_logic_vector (NBIT -1 downto 0));
-	end component;
-	
-	component decoder
-	port (waddr: IN std_logic_vector(NSEL-1 downto 0);
-		  dec_out: OUT std_logic_vector(2**NBIT-1 downto 0));
-	end component;
-		  
-	component mux
-	PORT (input : IN reg_to_array;
-		opsel : IN STD_LOGIC_VECTOR (NSEL-1 DOWNTO 0);
-		Y : OUT std_logic_vector (NSEL-1 DOWNTO 0));
-	end component;
+	SIGNAL regarray : reg_to_array;
 	
 	BEGIN
-	
-	--Writing
-	decode: decoder port map (waddr, dec_out);
-	reg_gen: FOR i in 2**NSEL-1 downto 0 GENERATE
-		reg_array(i): reg port map (clk, '0', rst_s, '0', dec_out(i) AND we, wdata, mux_in(i));
-	end generate;
-	
-	--Reading
-	M1: mux port map (reg_array, raddr_1, rdata_1);
-	M2: mux port map (reg_array, raddr_2, rdata_2);
-	
-	
+		PROCESS (clk)
+		VARIABLE --Something here, I think it was a temporary variable used for writing
+		BEGIN
+			if (clk'EVENT AND clk='1' and rst_s='1') then
+				for i in (2**NSEL-1 downto 0) loop
+					regarray(i) <= (OTHERS=>'0')
+				end loop;
+			elsif clk'event and clk='1' then
+				--Code
+			end	if;
+		end process;
 	
 END Structural;
