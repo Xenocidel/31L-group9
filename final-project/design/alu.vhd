af79d3b537fd20ed0a4491a9b9a09e8856cb798a
Werdna629 <amehta@Andrews-MacBook-Pro.local>
2015-12-02 19:27:58 -0800

    Added old ALU and regfile

--- final-project/design/alu.vhd
+++ final-project/design/alu.vhd
@@ -1,9 +1,31 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.All;
-
-ENTITY aludec is --ALU control decoder
+ENTITY alu_32bit IS 
 	PORT(
-		funct: IN STD_LOGIC_VECTOR(5 DOWNTO 0);
-		aluop: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-		alucontrol: OUT STD_LOGIC_VECTOR(2 DOWNTO 0));
-END aludec;
+	A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+	B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+	opsel : IN STD_LOGIC_VECTOR(2 DOWNTO 0); mode : IN STD_LOGIC;
+	output1 : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+	cout : OUT STD_LOGIC );
+	END alu_32bit;
+	
+ARCHITECTURE Structural OF alu_32bit IS
+	COMPONENT ALU_1bit
+		PORT(A, B, mode, cin : IN STD_LOGIC; opsel : IN STD_LOGIC_VECTOR; output1, cout: OUT STD_LOGIC);
+	END COMPONENT;
+	
+	SIGNAL cin, optype : STD_LOGIC;
+	SIGNAL coutbuff: STD_LOGIC_VECTOR(31 DOWNTO 0);
+BEGIN
+	cin <=  '1' WHEN opsel = "100" OR opsel = "110" OR opsel = "011" ELSE
+			'0';
+			
+	ALUXBASE : ALU_1bit port map(A(0), B(0), mode, cin, opsel, output1(0), coutbuff(0));
+	
+	G1: FOR i IN 1 TO 31 GENERATE
+		
+		ALUX: ALU_1bit port map(A(i), B(i), mode, coutbuff(i-1), opsel, output1(i), coutbuff(i));
+	END GENERATE G1;
+	
+	cout <= coutbuff(31);	
+END Structural;
--- final-project/design/regfile.vhd
+++ final-project/design/regfile.vhd
@@ -1,11 +1,38 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.All;
 
-ENTITY regfile is --three-port register file
-	PORT(
-		clk: IN STD_LOGIC;
-		we3: IN STD_LOGIC;
-		ra1, ra2, wa3: IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-		wd3: IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-		rd1, rd2: OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
-END regfile;
+ENTITY regfile IS
+	GENERIC(NBIT : INTEGER := 32;
+			NSEL : INTEGER := 3);
+	PORT (
+		clk : IN std_logic ;
+		rst_s : IN std_logic ; -- synchronous reset
+		we : IN std_logic ; -- write enable
+		raddr_1 : IN std_logic_vector (NSEL -1 DOWNTO 0); -- read address 1
+		raddr_2 : IN std_logic_vector (NSEL -1 DOWNTO 0); -- read address 2
+		waddr : IN std_logic_vector (NSEL -1 DOWNTO 0); -- write address
+		rdata_1 : OUT std_logic_vector (NBIT -1 DOWNTO 0); -- read data 1
+		rdata_2 : OUT std_logic_vector (NBIT -1 DOWNTO 0); -- read data 2
+		wdata : IN std_logic_vector (NBIT -1 DOWNTO 0) -- write data 1
+		);
+END regfile ;
+
+ARCHITECTURE Structural OF regfile IS
+	
+	type reg_to_array is array (2**NSEL-1 downto 0) of std_logic_vector(NBIT-1 downto 0); 
+	SIGNAL regarray : reg_to_array;
+	
+	BEGIN
+		PROCESS (clk)
+		VARIABLE --Something here, I think it was a temporary variable used for writing
+		BEGIN
+			if (clk'EVENT AND clk='1' and rst_s='1') then
+				for i in (2**NSEL-1 downto 0) loop
+					regarray(i) <= (OTHERS=>'0')
+				end loop;
+			elsif clk'event and clk='1' then
+				--Code
+			end	if;
+		end process;
+	
+END Structural;
