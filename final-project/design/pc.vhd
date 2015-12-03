a2de6ba84ccb0126b0919f061701bd20fb2db8fe
Xenocidel <aliao4@uci.edu>
2015-12-02 19:37:35 -0800

    changed to async rst

---
+++ final-project/design/pc.vhd
@@ -0,0 +1,34 @@
+LIBRARY ieee; 
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY counter IS
+	GENERIC (NBIT: INTEGER := 32;
+				STEP: INTEGER := 1);
+	PORT(
+				clk : IN STD_LOGIC;
+				rst_a : IN STD_LOGIC;
+				preload : IN STD_LOGIC;
+				din : IN STD_LOGIC_VECTOR(NBIT-1 DOWNTO 0);
+				dout : OUT STD_LOGIC_VECTOR(NBIT-1 DOWNTO 0)
+	);
+END counter;
+
+ARCHITECTURE Behavioral OF counter IS
+BEGIN
+		PROCESS(clk, rst_a)
+			VARIABLE temp: STD_LOGIC_VECTOR(NBIT-1 DOWNTO 0);
+		BEGIN
+			IF (rst_a = '1') THEN												
+					temp := (OTHERS => '0');									--Reset the counter
+			END IF
+			IF (clk'EVENT AND clk = '1') THEN
+				IF (preload = '1') THEN										
+					temp := din;												--Load din into counter register
+				ELSE											
+					temp := std_logic_vector((unsigned(temp)) + to_unsigned(STEP, NBIT)); --Count up
+				END IF
+			END IF;
+			dout <= temp;
+		END PROCESS;
+END Behavioral;		
