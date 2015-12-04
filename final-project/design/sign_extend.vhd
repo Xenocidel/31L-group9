12d14f3904c592ded00e39609e0792e9fa42481c
Xenocidel <aliao4@uci.edu>
2015-12-03 22:12:20 -0800

    fixed sign extend
    hardcoded because unsure of correct usage of others

--- final-project/design/sign_extend.vhd
+++ final-project/design/sign_extend.vhd
@@ -1,21 +1,18 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.All;
 
-GENERIC (in_bits: INTEGER := 15;
-		 out_bits: INTEGER := 32);
-
 ENTITY sign_extend is
 	PORT(
 		rst: IN std_logic;
-		input: IN std_logic_vector(in_bits-1 downto 0);
-		output: OUT std_logic_vector(out_bits-1 downto 0)
+		input: IN std_logic_vector(15 downto 0);
+		output: OUT std_logic_vector(31 downto 0)
 		);
 END sign_extend;
 
 ARCHITECTURE Structural of sign_extend is
 BEGIN
-	output <= (others => '0' & input(in_bits-1 downto 0)) WHEN rst='0' and input(in_bits-1)='0' ELSE
-			  (others => '1' & input(in_bits-1 downto 0)) WHEN rst='0' and input(in_bits-1)='1' ELSE
+	output <= ("000000000000000000" & input(15 downto 0)) WHEN rst='0' and input(in_bits-1)='0' ELSE
+			  (others => "1111111111111111111" & input(15 downto 0)) WHEN rst='0' and input(in_bits-1)='1' ELSE
 			  (others => '0') WHEN rst='1';
 
 END Structural
