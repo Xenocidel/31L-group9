03a8898ce001447d7da1fadec16fcdf9efbdbbfd
Xenocidel <aliao4@uci.edu>
2015-12-03 19:36:28 -0800

    ...

---
+++ final-project/design/comp_pkg.vhd
@@ -0,0 +1,21 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.All;
+
+PACKAGE comp_pkg IS
+	--mips--
+	
+	--alu--
+	
+	--dma--
+	
+	--instruction memory (cache)--
+	
+	--pc--
+	
+	--sign extender--
+	
+	--regfile--
+	
+	--mux--
+	
+END comp_pkg;
